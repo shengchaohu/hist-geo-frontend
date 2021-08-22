931db7602e739db12e6c2f09634635cb21812194
Shengchao Hu <shengchao@cs.uchicago.edu>
2021-08-22 18:35:13 -0500

    add mark cluster and use addgeojson instead of loadgeojson

--- src/index.js
+++ src/index.js
@@ -19,14 +19,18 @@ import "./style.css";
 import 'core-js/es/map';
 import 'core-js/es/set';
 
+import axios from 'axios';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ButtonAppBar from "./appBar";
 
 
 let map;
-let markers = new Map();
+let markers_list = [];
+let markers_map = new Map();
 let all_features = [];
+let mark_cluster;
 
 
 function initMap() {
@@ -37,59 +41,72 @@ function initMap() {
     });
     
     // default rendering
-    // generate_map(1368, 4329)
-    
-    ReactDOM.render(
-        <ButtonAppBar features={all_features}/>,
-        document.getElementById('app_bar')
-    );
+    generate_map(1368, 4329)
 }
 
 function clear_markers() {
-    for (let [k, v] of markers) {
+    for (let [k, v] of markers_map) {
         // must do both to remove a marker
         v.setMap(null);
         map.data.remove(k)
     }
-    markers = new Map();
+    markers_map = new Map();
+    all_features = [];
+    markers_list = [];
+    mark_cluster && mark_cluster.clearMarkers();
+}
+
+function restore_map_options() {
+    map.setOptions({
+        zoom: 5,
+        center: new google.maps.LatLng(34.5, 108.5),
+        mapTypeId: "terrain",
+    })
 }
 
 function generate_map(year, dynasty_id) {
-    clear_markers()
+    clear_markers();
+    restore_map_options();
     
-    map.data.loadGeoJson(`http://73.36.166.39:18880/administrative-divisions/?year=${year}&dynasty_id=${dynasty_id}`, null, features => {
-        features.map(feature => {
-            feature.toGeoJson((feature_geo_json) => {
-                let coordinates = feature_geo_json.geometry.coordinates
-                let my_LatLng = new google.maps.LatLng(coordinates[1], coordinates[0])
+    axios.get(`http://73.36.166.39:18880/administrative-divisions/?year=${year}&dynasty_id=${dynasty_id}`)
+        .then(function (response) {
+            
+            all_features = map.data.addGeoJson(response.data);
+            map.data.setStyle({visible: false});
+            
+            // handle_marker_window(map);
+            
+            markers_list = all_features.map(feature => {
                 let marker = new google.maps.Marker({
-                    position: my_LatLng,
+                    position: feature.getGeometry().get(0),
+                    data: feature,
                     map: map,
-                    title: feature_geo_json.properties.description
+                    title: feature.getProperty("description"),
                 });
-                markers.set(feature, marker);
-                all_features.push(feature);
-            })
+                markers_map.set(feature, marker);
+                handle_marker_window(marker)
+                return marker
+            });
+            
+            // TODO: can add more interesting options like onmouseover
+            mark_cluster = new MarkerClusterer(map, markers_list, {
+                imagePath:
+                    "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+                maxZoom: 9,
+                minimumClusterSize: 3,
+            });
+            
+            ReactDOM.render(
+                <ButtonAppBar features={all_features}/>,
+                document.getElementById('app_bar')
+            );
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-    });
-    
-    // render styling (color and so on)
-    // map.data.setStyle(feature => {
-    //   let letter = feature.getProperty('letter');
-    //   let color = "gray";
-    //   if (["G", "e"].includes(letter)) {
-    //     color = "green";
-    //   }
-    //   return {
-    //     fillColor: color,
-    //     strokeWeight: 1
-    //   }
-    // });
-    
-    handle_marker_window(map)
 }
 
-function zoom_to_marker(marker, zoom_level = 6) {
+function zoom_to_marker(marker, zoom_level = 9) {
     map.setCenter(marker.getPosition())
     map.setZoom(zoom_level);
 }
@@ -102,24 +119,42 @@ function animate_marker(marker, animation = google.maps.Animation.BOUNCE) {
     }
 }
 
-// still some logic error with mouse handling...
-function handle_marker_window(map) {
+// still some logic error with mouse handling...maybe due to async
+// function handle_marker_window(map) {
+//     let info_window = new google.maps.InfoWindow();
+//
+//     // Set event listener for each feature.
+//     // from https://stackoverflow.com/a/24052168
+//     map.data.addListener('click', function (event) {
+//         info_window.setContent(event.feature.getProperty('ch_name') + "<br>" + event.feature.getProperty('admin_type') + "<br>" + event.feature.getProperty('description'));
+//         info_window.setPosition(event.latLng);
+//         info_window.setOptions({pixelOffset: new google.maps.Size(0, -34)});
+//         info_window.open(map);
+//     });
+// }
+
+function handle_marker_window(marker) {
     let info_window = new google.maps.InfoWindow();
     
     // Set event listener for each feature.
     // from https://stackoverflow.com/a/24052168
-    map.data.addListener('mouseover', function (event) {
-        info_window.setContent(event.feature.getProperty('ch_name') + "<br>" + event.feature.getProperty('admin_type') + "<br>" + event.feature.getProperty('description'));
-        info_window.setPosition(event.latLng);
+    marker.addListener('mouseover', function () {
+        let feature = marker.data;
+        info_window.setContent(feature.getProperty('ch_name') + "<br>" + feature.getProperty('admin_type') + "<br>" + feature.getProperty('description'));
+        info_window.setPosition(marker.position);
         info_window.setOptions({pixelOffset: new google.maps.Size(0, -34)});
         info_window.open(map);
+    });
+    
+    marker.addListener('mouseout', function () {
+        info_window.close();
     });
 }
 
 export {initMap};
 export {generate_map};
 export {zoom_to_marker, animate_marker};
-export {markers};
+export {markers_map};
 
 
 
--- src/searchBox.js
+++ src/searchBox.js
@@ -1,23 +1,50 @@
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
 import React from "react";
-import {markers, zoom_to_marker, animate_marker} from "./index";
+import {markers_map, zoom_to_marker, animate_marker} from "./index";
+import PropTypes from "prop-types";
+import {withStyles, makeStyles} from "@material-ui/core/styles";
 
-// can update to use async searchbox later
+
+const useStyles = makeStyles(theme => ({
+    inputRoot: {
+        color: "black",
+        fontFamily: "Roboto Mono",
+        backgroundColor: "#f2f2f2",
+        "& .MuiOutlinedInput-notchedOutline": {
+            borderWidth: "2px",
+            borderColor: "blue"
+        },
+        "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderWidth: "2px",
+            borderColor: "blue"
+        },
+        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderWidth: "2px",
+            borderColor: "blue"
+        }
+    }
+}));
+
+
+// TODO: update to use async searchbox later
 export default function SearchBox(props) {
+    const classes = useStyles();
+    
     return (
         <Autocomplete
             id="grouped-demo"
             options={props.features}
+            classes={classes}
             // groupBy={(option) => option.firstLetter}
             onChange={(event, value) => console.log(value)}
             onHighlightChange={(event, value) => {
-                // event && animate_marker(markers.get(value));
-                event && zoom_to_marker(markers.get(value));
+                event && animate_marker(markers_map.get(value));
+                event && zoom_to_marker(markers_map.get(value));
             }}
             getOptionLabel={(feature) => feature.getProperty('description')}
             style={{width: 300}}
-            renderInput={(params) => <TextField {...params} label="With categories" variant="outlined"/>}
+            renderInput={(params) => <TextField {...params} label="Search on the map..." variant="outlined"/>}
         />
     );
 }
